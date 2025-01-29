3305d16087afea385942ce6fd5ee999da04cce04
Frances Dionne Maglasang <dionnemaglasang@gmail.com>
2025-01-29 23:17:53 +0800

    Time: 0 ms (100.00%) | Memory: 42.1 MB (46.78%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    public int removeElement(int[] nums, int val)
+    {
+        int ctr = 0;
+
+        for(int i=0; i<nums.length; i++)
+        {
+            if(nums[i] == val)
+            {
+                continue;
+            }
+            else
+            {
+                nums[ctr] = nums[i];
+                ctr++;
+            }
+        }
+        return ctr;
+    }
+}
